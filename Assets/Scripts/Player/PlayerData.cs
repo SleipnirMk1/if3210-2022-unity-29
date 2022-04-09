51afb2ca4aad20bce27221464c1737561aa7d936
IlyasaS <13519023@std.stei.itb.ac.id>
2022-04-10 01:32:45 +0700

    functional menu and scoreboard, design to be improved

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -8,8 +8,14 @@ public class PlayerData : MonoBehaviour
     // Variables //
     public static PlayerData instance;      // Singleton
 
-    public string playerName = "Joko";      // Buat nama player, mungkin namanya mo bisa ganti"
-    public int score;
+    [SerializeField]
+    private string playerName = "Joko";      // Buat nama player, mungkin namanya mo bisa ganti"
+
+    public List<ZenScore> zenScoreBoard = new List<ZenScore>();
+    public List<WaveScore> waveScoreBoard = new List<WaveScore>();
+
+    [SerializeField]
+    private int listMaxElement = 3;
 
     // Functions //
     private void Awake()                    // Biar duluan klo ada proses lain yg pake void start
@@ -25,6 +31,56 @@ public class PlayerData : MonoBehaviour
             Destroy(gameObject);
             Debug.Log("Destroyed instance");
         }
+
+        ZenScore testZen = new ZenScore();
+        testZen.name = "fulan";
+        testZen.time = 20;
+        zenScoreBoard.Add(testZen);
+
+        WaveScore waveTest = new WaveScore();
+        waveTest.name = "fulan2";
+        waveTest.score = 20;
+        waveTest.maxWaves = 5;
+        waveScoreBoard.Add(waveTest);
     }
 
+    public void setPlayerName(string name)
+    {
+        playerName = name;
+    }
+
+    public void addZenScoreBoard(ZenScore newData)
+    {
+        if (zenScoreBoard.Count >= listMaxElement) {
+            foreach (var elmt in zenScoreBoard) {
+                if (elmt.time < newData.time) {
+                    zenScoreBoard.Remove(elmt);
+                }
+            }
+        }
+
+        zenScoreBoard.Add(newData);
+    }
+
+    public void addWaveScoreBoard(WaveScore newData)
+    {
+        if (waveScoreBoard.Count >= listMaxElement) {
+            foreach (var elmt in waveScoreBoard) {
+                if (elmt.score < newData.score) {
+                    waveScoreBoard.Remove(elmt);
+                }
+            }
+        }
+        waveScoreBoard.Add(newData);
+    }
+
+    public List<ZenScore> getZenScoreBoard()
+    {
+        return zenScoreBoard;
+    }
+
+    public List<WaveScore> getWaveScoreBoard()
+    {
+        return waveScoreBoard;
+    }
 }
