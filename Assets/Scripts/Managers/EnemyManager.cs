a830daf0e16567b7e6731326d3cd0114da2a517b
imulki <64567731+imulki@users.noreply.github.com>
2022-04-10 15:47:56 +0700

    update wave system

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -10,6 +10,7 @@ public class EnemyManager : MonoBehaviour{
     public float waveSpawnInterval = 1f;
     public WeaponUpgradeManager weaponUpgradeManager;
     public ScoreManager scoreManager;
+    public int maxWave = 3;
 
     // Zen Incremental Difficulty;
     public int zenLevel = 1; 
@@ -68,7 +69,10 @@ public class EnemyManager : MonoBehaviour{
             int remainingEnemy = GameObject.FindGameObjectsWithTag("Enemy").Length;
             if ((remainingEnemy <= 0))
             {
-                if (((currentWaveIdx % 3) == 0) && (currentWaveIdx != 0) )
+                if ((currentWaveIdx == maxWave) && (playerHealth.currentHealth > 0))
+                {
+                    playerHealth.TakeDamage(1000);
+                } else if (((currentWaveIdx % 3) == 0) && (currentWaveIdx != 0) )
                 {
                     if (!weaponUpgradeManager.gameObject.activeSelf)
                     {
